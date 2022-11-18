2385452ff8495565d48f105e18c973690e5d078d
Paul Joseph <paul.joseph@aon.at>
2022-11-18 16:11:33 +0100

    quick save

--- Code/ros_ws/src/pi_hub_srvs/build/pi_hub_srvs/pi_hub_srvs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/ros_ws/src/pi_hub_srvs/build/pi_hub_srvs/pi_hub_srvs__py
-# Build directory: /root/ros_ws/src/pi_hub_srvs/build/pi_hub_srvs/pi_hub_srvs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Code/ros_ws/src/pump_control/pump_control/pump_control_node.py
+++ Code/ros_ws/src/pump_control/pump_control/pump_control_node.py
@@ -1,5 +1,33 @@
-def main():
-    print('Hi from pump_control.')
+# ROS libs
+import rclpy
+from rclpy.node import Node
+
+from  import Bool
+
+# lets create a class for the pump controler
+class PumpControler(Node):
+
+    def __init__(self):
+        super().__init__('pump_controler')
+
+        self.srv = self.create_service(PumpOn, 'pump_on', self.pumpOn)
+
+        self.subscription = self.create_subscription(
+                    Bool,
+                    'pump_control',
+                    self.pump_control_callback,
+                    10)
+        self.subscription  # prevent unused variable warning
+        self.i = 0
+
+    def pump_control_callback(self, msg):
+        self.get_logger().info('I heard: "%s"' % msg.data)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Node('pump_control_node')
+    rclpy.spin(node)
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
