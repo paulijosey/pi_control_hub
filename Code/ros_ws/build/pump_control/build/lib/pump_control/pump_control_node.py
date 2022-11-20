000e8baaf71327c773f7e5521ce43cae47d01546
Paul Joseph <paul.joseph@aon.at>
2022-11-20 23:07:14 +0100

    implemented common config file

--- Code/ros_ws/build/pump_control/build/lib/pump_control/pump_control_node.py
+++ Code/ros_ws/build/pump_control/build/lib/pump_control/pump_control_node.py
@@ -5,17 +5,28 @@ from rclpy.node import Node
 import time
 import lgpio
 
-# TODO: read this from config
-PUMP_HEADER = 15
-
 from pi_hub_srvs.srv import Control     # check service definition in pi_hub_srvs
 
 # lets create a class for the pump controler
 class PumpController(Node):
 
-    def __init__(self, header_open_):
-        super().__init__('pump_controller')
-        self.header_open = header_open_
+    def __init__(self):
+        super().__init__('pump_control_node')
+        
+        # decalre ROS parameters
+        self.declare_parameters(
+        namespace='',
+        parameters=[
+            ('gpio', 4)
+        ])
+
+        # set the parameters to values
+        self.gpio = self.get_parameter('gpio').get_parameter_value().integer_value
+        # open the gpio chip and set the LED pin as output
+        self.header_open = lgpio.gpiochip_open(0)
+        lgpio.gpio_claim_output(self.header_open, self.gpio)
+
+
         # init service that uses:
         #       "Control" service type,
         #       "pump_control" service name (thats the one we will use on ROS to call it)
@@ -49,7 +60,7 @@ class PumpController(Node):
         '''
         # TODO
         # Turn the GPIO pin on
-        lgpio.gpio_write(self.header_open, PUMP_HEADER, 1)
+        lgpio.gpio_write(self.header_open, self.gpio, 1)
         self.get_logger().info('Turn pump on')
         return True
 
@@ -60,7 +71,7 @@ class PumpController(Node):
         '''
         # TODO
         # Turn the GPIO pin off
-        lgpio.gpio_write(self.header_open, PUMP_HEADER, 0)
+        lgpio.gpio_write(self.header_open, self.gpio, 0)
         self.get_logger().info('Turn pump off')
         return True
 
@@ -68,11 +79,8 @@ class PumpController(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    # open the gpio chip and set the LED pin as output
-    header_open = lgpio.gpiochip_open(0)
-    lgpio.gpio_claim_output(header_open, PUMP_HEADER)
     # Start the service
-    pump_control_service = PumpController(header_open)
+    pump_control_service = PumpController()
     rclpy.spin(pump_control_service)
     rclpy.shutdown()
 
--- Code/ros_ws/src/time_schedule/time_schedule/time_schedule_node.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print('Hi from time_schedule.')
-
-
-if __name__ == '__main__':
-    main()
