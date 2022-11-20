d56e07aa201a100374c280b35efa5e3dcbaa019a
Paul Joseph <paul.joseph@aon.at>
2022-11-20 23:34:01 +0100

    documentation

--- Code/ros_ws/install/pump_control/lib/python3.10/site-packages/pump_control/pump_control_node.py
+++ Code/ros_ws/install/pump_control/lib/python3.10/site-packages/pump_control/pump_control_node.py
@@ -1,3 +1,12 @@
+# @Author: Paul Joseph
+# @Email: paul.joseph241@gmail.com
+# @Date: 2022-11-20 22:23:15
+# @Last Modified by:   undefined
+# @Last Modified time: 2022-11-20 22:23:15
+# @Description: file:///root/ros_ws/src/pump_control/pump_control/pump_control_node.py
+#               Handles the control of the water pump. Common functions like turning on/off 
+#               the pump and returning the current operation status.
+
 # ROS libs
 import rclpy
 from rclpy.node import Node
@@ -9,30 +18,50 @@ from pi_hub_srvs.srv import Control     # check service definition in pi_hub_srv
 
 # lets create a class for the pump controler
 class PumpController(Node):
-
+    #     ____                _                   _             
+    #    / ___|___  _ __  ___| |_ _ __ _   _  ___| |_ ___  _ __ 
+    #   | |   / _ \| '_ \/ __| __| '__| | | |/ __| __/ _ \| '__|
+    #   | |__| (_) | | | \__ \ |_| |  | |_| | (__| || (_) | |   
+    #    \____\___/|_| |_|___/\__|_|   \__,_|\___|\__\___/|_|   
     def __init__(self):
         super().__init__('pump_control_node')
         
+        # get ROS params
+        self.init_ros_parameters()
+        # open gpio headers
+        self.init_gpio_headers()
+        # initialize services
+        self.init_services()
+ 
+    #    ____   ___  ____    _____                 _   _                 
+    #   |  _ \ / _ \/ ___|  |  ___|   _ _ __   ___| |_(_) ___  _ __  ___ 
+    #   | |_) | | | \___ \  | |_ | | | | '_ \ / __| __| |/ _ \| '_ \/ __|
+    #   |  _ <| |_| |___) | |  _|| |_| | | | | (__| |_| | (_) | | | \__ \
+    #   |_| \_\\___/|____/  |_|   \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
+    def init_ros_parameters(self):
+        '''
+        Load all ROS parameters that we need for this node.
+        Loading from a config file will be handled in the launch file
+        '''
         # decalre ROS parameters
-        self.declare_parameters(
-        namespace='',
-        parameters=[
-            ('gpio', 4)
-        ])
+        self.declare_parameter('gpio', 15)
 
         # set the parameters to values
         self.gpio = self.get_parameter('gpio').get_parameter_value().integer_value
-        # open the gpio chip and set the LED pin as output
-        self.header_open = lgpio.gpiochip_open(0)
-        lgpio.gpio_claim_output(self.header_open, self.gpio)
 
+        # log output of params we are using
+        self.get_logger().info("gpio: %s," %
+                                (str(self.gpio)))
 
+    def init_services(self):
+        '''
+        initialize all service handled by this node.
+        '''
         # init service that uses:
         #       "Control" service type,
         #       "pump_control" service name (thats the one we will use on ROS to call it)
         #       and call function "control_callback()"
         self.srv = self.create_service(Control, 'pump_control', self.control_callback)
-
 
     def control_callback(self, request, response):
         '''
@@ -52,13 +81,31 @@ class PumpController(Node):
             self.get_logger().info('Incoming request\nstate: %d is invalid' % (request.state))
             response.success = False
         return response
+    
+    def publish_status(self):
+        # TODO
+        self.get_logger().warn("publish_status has not yet been implemented!")
+
+    #     ____ ____ ___ ___     ____            _             _     
+    #    / ___|  _ \_ _/ _ \   / ___|___  _ __ | |_ _ __ ___ | |___ 
+    #   | |  _| |_) | | | | | | |   / _ \| '_ \| __| '__/ _ \| / __|
+    #   | |_| |  __/| | |_| | | |__| (_) | | | | |_| | | (_) | \__ \
+    #    \____|_|  |___\___/   \____\___/|_| |_|\__|_|  \___/|_|___/
+    def init_gpio_headers(self):
+        '''
+        open all the gpio headers that need to be controlled by this node.
+        Should be called in the node init phase (the constructor).
+        Make sure you already have the ROS params loaded.
+        '''
+        # open the gpio chip and set the pump pin as output
+        self.header_open = lgpio.gpiochip_open(0)
+        lgpio.gpio_claim_output(self.header_open, self.gpio)
 
     def pumpOn(self) -> bool:
         '''
         Turn on the pump (define the GPIO header of the pump in the 
         config file)
         '''
-        # TODO
         # Turn the GPIO pin on
         lgpio.gpio_write(self.header_open, self.gpio, 1)
         self.get_logger().info('Turn pump on')
@@ -69,12 +116,10 @@ class PumpController(Node):
         Turn off the pump (define the GPIO header of the pump in the 
         config file)
         '''
-        # TODO
         # Turn the GPIO pin off
         lgpio.gpio_write(self.header_open, self.gpio, 0)
         self.get_logger().info('Turn pump off')
         return True
-
 
 
 def main(args=None):
